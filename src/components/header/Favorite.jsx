201a32d15f838ee2ba5f6ff62b7d7bccd8d70f3d
SumonsNote <sohankhance@gmail.com>
2024-02-12 15:09:59 +0600

    added the full completed weatherdashboard

---
+++ src/components/header/Favorite.jsx
@@ -0,0 +1,13 @@
+import heartIcon from "../../assets/heart.svg";
+
+export default function Favourite({ onShow }) {
+  return (
+    <div
+      onClick={onShow}
+      className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all"
+    >
+      <img src={heartIcon} alt="Heart Icon" />
+      <span>Favourite Locations</span>
+    </div>
+  );
+}
---
+++ src/provider/index.js
@@ -0,0 +1,5 @@
+import FavoriteProvider from "./FavoriteProvider.jsx";
+import LocationProvider from "./LocationProvider.jsx";
+import WeatherProvider from "./WeatherProvider.jsx";
+
+export { FavoriteProvider, LocationProvider, WeatherProvider };
